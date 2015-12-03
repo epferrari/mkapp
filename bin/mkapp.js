e4ecc6c9c3685b27a02029219992ea4ba4fed053
Ethan Ferrari <ethan@onefiremedia.com>
2015-12-02 18:29:06 -0600

    support for finding local version of mkapp package with resolve

--- bin/mkapp.js
+++ bin/mkapp.js
@@ -1,73 +1,14 @@
 #!/usr/bin/env node
 
+var mkapp;
 var resolve = require('resolve');
-console.log( resolve.sync('bluebird',{basedir: process.cwd()}) );
-console.log( resolve.sync('babel',{basedir: process.cwd()}) );
 try {
-	console.log( resolve.sync('cli-color',{basedir: process.cwd()}) );
-	console.log( resolve.sync('mkapp',{basedir: process.cwd()}) );
+	mkapp = require( resolve.sync('mkapp',{basedir: process.cwd()}) );
+	console.log('Using local mkapp');
 }
 catch(ex){
-	console.log('error finding modules');
+	console.log('Cannot find local mkapp, defaulting to global. \nInstall locally if you are experiencing dependency errors.\n---');
+	mkapp = require('../lib/mkapp.js');
 }
-
-
-var Promise = require('bluebird');
-var clc = require('cli-color');
-var mkapp = require('commander');
-
-var init = require('../lib/mkapp-init.js');
-var go = require('../lib/mkapp-go.js');
-var clean = require('../lib/mkapp-clean.js');
-var transpile = require('../lib/mkapp-transpile.js');
-var pkg = require('../package.json');
-
-mkapp.version(pkg.version);
-
-
-mkapp
-	.command('new')
-	.description('Create a new application with a directory scaffold, download boilerplate project, and install dependencies.')
-	.action(function(){
-		init(pkg.version);
-	});
-
-mkapp
-	.command('clean <dir>')
-	.description('Remove a directory and its contents with rm -rf <dir>')
-	.action(function(dir){
-		clean(dir);
-	});
-
-mkapp
-	.command('transpile')
-	.description('Transpile ES6 modules in your server app')
-	.action(transpile);
-
-mkapp
-	.command('go')
-	.description('Build app and run at http://localhost:<process.env.PORT>. Defaults to 3030')
-	.action(go);
-
-mkapp
-	.command('build')
-	.description('Build a minified distribution bundle of your app.')
-	.action(function(){
-		console.log('build command coming soon!')
-	});
-
-mkapp
-	.command('dist <message> [branch]')
-	.description('build, commit, and push your app to <branch> '+
-		'of your repository. If <branch> is left blank, the bundle '+
-		'will be commited to `origin development`')
-	.action(function(message,branch){
-		if(!message){
-			console.log(clc.red('Must specify a commit message'));
-		}else{
-			console.log('dist command coming soon!');
-		}
-	});
-
 
 mkapp.parse(process.argv);
