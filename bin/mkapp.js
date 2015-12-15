b49de05e60473e9aa77e77fb7e6036f3490acc37
Ethan Ferrari <ethan@onefiremedia.com>
2015-12-15 02:48:23 -0600

    reference to the cli app at index.js

--- bin/mkapp.js
+++ bin/mkapp.js
@@ -2,13 +2,14 @@
 
 var mkapp;
 var resolve = require('resolve');
+
 try {
 	mkapp = require( resolve.sync('mkapp',{basedir: process.cwd()}) );
 	console.log('Using local mkapp');
 }
 catch(ex){
-	console.log('Cannot find local mkapp, defaulting to global. \nInstall locally if you are experiencing dependency errors.\n---');
-	mkapp = require('../lib/mkapp.js');
+	console.log('Cannot find local mkapp, defaulting to global. \nInstall locally if you experience dependency issues.\n---');
+	mkapp = require('../index.js');
 }
 
 mkapp.parse(process.argv);
