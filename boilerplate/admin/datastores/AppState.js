7d7de83e5455fda5525984dfd3c825a3a80cf7db
Ethan Ferrari <ethan@onefiremedia.com>
2015-12-22 09:52:27 -0600

    using immutable state constructor with ImmutableJS for app state

--- boilerplate/admin/datastores/AppState.js
+++ boilerplate/admin/datastores/AppState.js
@@ -2,30 +2,26 @@ import Reflux from 'reflux';
 import appActions from '../actions';
 import {merge,contains} from 'lodash';
 import {typeOf} from '@epferrari/js-utils';
+import State from '@epferrari/immutable-state';
 
 import ConduxClient from 'condux-client';
-import conduxService from '../../condux/admin';
-
+import conduxService from '../../condux/public';
 
 var initialState = {
 	currentPath: 'home',
 	wsConnection: 0,
 	inLoadingState: false,
-	navbarColor: 'transparent',
-	showNavbarTitle: false
+	navbarColor: undefined,
+	showNavbarTitle: false,
+	materialNav: false
 };
 
-var $state = {};
-
-
-function setState(newState){
-	$state = merge({},$state,newState);
-	stateStore.trigger($state);
-}
+var app = new State(initialState);
 
 
 var stateStore = Reflux.createStore({
 	listenables: [appActions],
+
 	onSET_CONNECTION_STATUS(status){
 
 		var ns = [
@@ -33,80 +29,61 @@ var stateStore = Reflux.createStore({
 			ConduxClient.CONNECTING,
 			ConduxClient.DISCONNECTED
 		];
-
 		if(ns.indexOf(status) !== -1){
-			setState({
+			let newState = app.setState({
 				wsConnection: status,
 				inLoadingState: (status === ConduxClient.CONNECTING)
 			});
+			this.trigger(newState);
 		}
 	},
 
 	onDID_NAVIGATE(payload){
-		if(payload.path !== $state.currentPath){
+		if(payload.path !== app.state.currentPath){
+			// attempt reconnection to condux service upon navigating to a new view
 			if(!conduxService.connected && !conduxService.connecting) conduxService.reconnect();
-			setState({
+
+			let newState = app.setState({
 				currentPath: payload.path,
 				viewTitle: payload.viewTitle,
 				backButtonLink: payload.backButtonLink
 			});
+
+			this.trigger(newState);
 		}
 	},
 
 	onENTER_LOADING_STATE(){
-		if(!$state.inLoadingState){
-			setState({inLoadingState: true});
+		if(!app.state.inLoadingState){
+			this.trigger( app.setState({inLoadingState: true}) );
 		}
 	},
 
 	onEXIT_LOADING_STATE(){
-		if($state.inLoadingState && !$state.wsConnection === ConduxClient.CONNECTING){
-			setState({inLoadingState: false});
+		// remove a temporary loading state
+		if(app.state.inLoadingState && app.state.wsConnection !== ConduxClient.CONNECTING){
+			this.trigger( app.setState({inLoadingState: false}) );
 		}
 	},
 
 	onSET_NAVBAR_COLOR(color){
-		if($state.navbarColor !== color){
-			setState({navbarColor: color});
+		if(app.state.navbarColor !== color){
+			this.trigger( app.setState({navbarColor: color}) );
 		}
 	},
 
 	onSHOW_NAVBAR_TITLE(){
-		if(!$state.showNavbarTitle) setState({showNavbarTitle: true});
+		if(!app.state.showNavbarTitle) this.trigger( app.setState({showNavbarTitle: true}) );
 	},
 
 	onHIDE_NAVBAR_TITLE(){
-		if($state.showNavbarTitle) setState({showNavbarTitle: false});
+		if(app.state.showNavbarTitle) this.trigger( app.setState({showNavbarTitle: false}) );
 	},
 
-	getState(filter){
-		if(!arguments.length){
-			return merge({},$state);
-		}else{
-			var keys = [];
-			if(typeOf(filter) === 'string'){
-				keys = [].slice.call(arguments,0);
-				// return the single value
-				if(keys.length === 1) return $state[filter];
-			}else if(typeOf(filter) === 'array'){
-				keys = filter;
-			}else if(typeOf(filter) === 'object'){
-				keys = Object.keys(filter);
-			}
-			if(keys.length){
-				// return reduced $state object with only keys/values of arguments passed to getState
-				return keys.reduce((r,k) => {
-					r[k] = $state[k];
-					return r;
-				},{});
-			}else{
-				// default to returning a merged copy of the entire $state
-				return merge({},$state);
-			}
-		}
+	getState(){
+		return app.state;
 	}
 });
 
-setState(initialState);
 
 export default stateStore;
