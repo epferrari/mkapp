ea3843743f64badb38ec3ecf539ce5d228a48f97
Ethan Ferrari <ethan@onefiremedia.com>
2015-12-26 18:18:33 -0600

    moved AppNav menu buttons into separate files

---
+++ _components/AppNav/MenuButtonRight.jsx
@@ -0,0 +1,57 @@
+import React
+  from 'react';
+import Touchable
+  from '../Touchable';
+import Column
+  from '../utility/Column';
+import FontIcon
+  from '@epferrari/react-fa-icon';
+
+
+var buttonStyles = {
+  margin:0,
+  padding:0,
+  paddingTop:4,
+  border:"none",
+  background:"none",
+  float:"right",
+  paddingBottom:0
+};
+
+var MenuButtonRight = React.createClass({
+  propTypes: {
+    show: React.PropTypes.bool,
+    onClick: React.PropTypes.func.isRequired,
+    label: React.PropTypes.string,
+    iconElement: React.PropTypes.element
+  },
+  getDefaultProps(){
+    return {
+      show: true,
+      iconElement: (<FontIcon icon="bars"/>)
+    }
+  },
+  render(){
+		if(this.props.show){
+			return (
+				<Touchable
+					component={Column}
+					width={3}
+					onClick={(e) => {
+						e.stopPropagation();
+						/*this.showMenu();*/
+            this.props.onClick(e);
+					}}
+					style={{paddingRight:8}}>
+					<button style={buttonStyles}>
+						{this.props.label} {this.props.iconElement}
+					</button>
+				</Touchable>
+			);
+		} else {
+			return <Column width={3}/>;
+		}
+	},
+});
+
+module.exports = MenuButtonRight;
