7cc67626cb4d38dd38806cfbaff809f67555f857
Ethan Ferrari <epferrari309@gmail.com>
2016-02-07 15:19:02 -0600

    added support for shared font and image assets between public and admin apps, moved fonts to shared asset folder

--- cli/copy.js
+++ cli/copy.js
@@ -3,15 +3,16 @@ var join = require('path').join;
 var Promise = require('bluebird');
 var clc = require('cli-color');
 var APP_ROOT = require('app-root-path').toString();
+var shell = require('shelljs');
 
 Promise.promisifyAll(fs);
 
 module.exports = function copy(context){
 
-	var config = require('./parse-config')();
-	var SRC_DIR = config.SRC_DIR;
-	var DEV_DIR = config.DEV_DIR;
-	var DIST_DIR = config.DIST_DIR;
+	var $$ = require('./parse-config')();
+	var SRC_DIR = $$.SRC_DIR;
+	var DEV_DIR = $$.DEV_DIR;
+	var DIST_DIR = $$.DIST_DIR;
 
 	if(context === 'dev'){
 		targetDir = DEV_DIR;
@@ -21,26 +22,56 @@ module.exports = function copy(context){
 		return contextError();
 	}
 
-	function copyFiles(scope){
-		console.log('copying '+scope+' static files to '+targetDir);
+	function copyGlobalFiles(scope,assetType,assetDirName){
+		console.log('copying global '+assetType+' to '+scope+'/assets/'+assetDirName);
+
+		var globalAssetPath = join(APP_ROOT,SRC_DIR,'assets',assetDirName);
+
+		return fs.ensureDirAsync(globalAssetPath)
+			.then(function(){
+				return new Promise(function(resolve,reject){
+
+					var localTargetDir = join(APP_ROOT,targetDir,scope,'assets',assetDirName);
+					var copyFiles = shell.cp('-Rf',join(globalAssetPath,'*'),localTargetDir);
+
+					if(!copyFiles || copyFiles && copyFiles.code !== 0){
+						console.log(clc.green('successfully copied global '+assetType+' to '+scope));
+						resolve();
+					}else{
+						reject('Could not copy global '+assetType+' to '+scope+'. Reason: '+copyFiles);
+					}
+				});
+			});
+	}
+
+	function copyLocalFiles(scope){
+		console.log('copying local static files and assets to '+scope)
 		return Promise.all([
 			fs.copyAsync(join(APP_ROOT,SRC_DIR,scope,'index.html'),join(APP_ROOT,targetDir,scope,'index.html')),
 			fs.copyAsync(join(APP_ROOT,SRC_DIR,scope,'assets'),join(APP_ROOT,targetDir,scope,'assets'))
 		]);
 	}
 
+	function copyAllFiles(scope){
+		return Promise.all([
+			copyGlobalFiles(scope,'fonts','fonts'),
+			copyGlobalFiles(scope,'images','img'),
+			copyLocalFiles(scope)
+		]);
+	}
+
 	return Promise.all([
-		copyFiles('public'),
-		(config.CREATE_ADMIN_APP ? copyFiles('admin') : Promise.resolve())
-	])
-	.then(function(){
-		console.log(clc.green('Successfully copied static assets'));
-	})
-	.catch(function(err){
-		var msg = "An error occured copying static files.";
-		console.log(clc.red(msg));
-		return Promise.reject(err);
-	});
+		copyAllFiles('public'),
+		($$.CREATE_ADMIN_APP ? copyAllFiles('admin') : Promise.resolve())
+		])
+		.then(function(){
+			console.log(clc.green('Successfully copied static assets'));
+		})
+		.catch(function(err){
+			var msg = "An error occured copying static files.";
+			console.log(clc.red(msg));
+			return Promise.reject(err);
+		});
 }
 
 function contextError(){
