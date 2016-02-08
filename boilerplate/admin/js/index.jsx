79c8f4d8a84b6c9ccd5613f196ebb960afd78447
Ethan Ferrari <epferrari309@gmail.com>
2016-02-07 20:36:27 -0600

    added babel polyfill and fixing syntax for safari

--- boilerplate/admin/js/index.jsx
+++ boilerplate/admin/js/index.jsx
@@ -2,5 +2,5 @@ if(process.env.NODE_ENV !== 'production'){
 	window.LiveReloadOptions = { host: 'localhost' };
 	require('livereload-js');
 }
-
+import 'babel-polyfill';
 import App from './app.jsx';
