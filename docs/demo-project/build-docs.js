0deca17b0be1272dba5de8e7afe1ff34d1a93c8e
Ethan Ferrari <epferrari309@gmail.com>
2016-02-13 19:30:11 -0600

    alignment fix, github-flavored doesnt like inline styles

--- docs/demo-project/build-docs.js
+++ docs/demo-project/build-docs.js
@@ -36,15 +36,19 @@ function constructPage(filename,header,footer){
 function getFooter(prev,next,n){
 	var output = ['\n\n','# ','\n\n'];
 
-	if(next){
-		output.push('<a href="./'+next+'#content" style="display:inline-block; padding:0 8px; text-align:right; float:right;" > Next >> </a>');
-	}
+	output.push('<a href="#top" style="display:inline-block; padding:0 8px; text-align:left; float:left;">Top</a>');
 
 	if(prev){
-		output.push('<a href="./'+prev+'#content" style="display:inline-block; padding:0 8px; text-align:left; float:right;" > << Previous </a>');
+		output.push('<a href="./'+prev+'#content" style="display:inline-block; padding:0 8px; text-align:left; float:left;" > << Previous </a>');
+	}
+	
+	if(next){
+		output.push('<a href="./'+next+'#content" style="display:inline-block; padding:0 8px; text-align:right; float:left;" > Next >> </a>');
 	}
 
-	output.push('<a href="#top" style="display:inline-block; padding:0 8px; text-align:left; float:left;">Top</a>');
+
+
+
 
 	output.push('\n\n');
 	return output.join('');
