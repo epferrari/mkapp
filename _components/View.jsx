787a668b012c728f8e893649892605f58f4ce3cc
Ethan Ferrari <epferrari309@gmail.com>
2016-06-10 00:39:34 -0500

    reapplying overflow scroll to viewframe
    wrapped viewframe in another div with relative positioning, seems not to break drawers.

--- _components/View.jsx
+++ _components/View.jsx
@@ -1,16 +1,16 @@
 import React
-  from 'react';
+	from 'react';
 import Container
-  from './gridsystem/Container';
+	from './gridsystem/Container';
 import {merge,pick}
-  from 'lodash';
+	from 'lodash';
 import MkappThemeMixin
-  from '../theme/mixin';
+	from '../theme/mixin';
 import MkappThemeStyleMerger
-  from '../theme/utils/styleMerger';
+	from '../theme/utils/styleMerger';
 
 function copy(object){
-  return merge({},object);
+	return merge({},object);
 }
 
 
@@ -23,61 +23,64 @@ function copy(object){
 * 	Good for drawers, modals, and fixed position elements like footers
 */
 const View = React.createClass({
-  mixins:[MkappThemeMixin],
+	mixins:[MkappThemeMixin],
 
-  propTypes:{
-    static: React.PropTypes.oneOfType([
-      React.PropTypes.element,
-      React.PropTypes.arrayOf(React.PropTypes.element)
-    ]),
-    navbarOffset: React.PropTypes.number
-  },
+	propTypes:{
+		static: React.PropTypes.oneOfType([
+			React.PropTypes.element,
+			React.PropTypes.arrayOf(React.PropTypes.element)
+		]),
+		navbarOffset: React.PropTypes.number
+	},
 
-  getDefaultProps(){
-    return {navbarOffset: 30};
-  },
+	getDefaultProps(){
+		return {navbarOffset: 30};
+	},
 
-  render(){
-    let styles = this.prepareStyles();
-    let propsStyles = merge({},this.props.style);
-    let bgStyles = pick(MkappThemeStyleMerger(this.props.style,this.getThemeStyles('view')),'backgroundColor');
-    delete propsStyles.backgroundColor;
-    return (
-      <div style={merge({},styles.viewframe,bgStyles)} ref="view">
-        <Container style={merge({},styles.container,propsStyles,styles.content)}>
-          {this.props.children}
-        </Container>
-        {this.props.static}
-      </div>
-    );
-  },
+	render(){
+		let styles = this.prepareStyles();
+		let propsStyles = merge({},this.props.style);
+		let bgStyles = pick(MkappThemeStyleMerger(this.props.style,this.getThemeStyles('view')),'backgroundColor');
+		delete propsStyles.backgroundColor;
+		return (
+			<div style={styles.outer}>
+				<div style={merge({},styles.viewframe,bgStyles)} ref="view">
+					<Container style={merge({},styles.container,propsStyles,styles.content)}>
+						{this.props.children}
+					</Container>
+				</div>
+				{this.props.static}
+			</div>
+		);
+	},
 
-  prepareStyles(){
-    let offsetTop = this.getThemeStyles('view').offsetTop || 0;
-    return copy({
-      viewframe: {
-        position: "absolute",
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        width: "100%",
-        overflowX: 'hidden',
-        overflowY: "scroll",
-        minHeight: global.innerHeight
-      },
-      container: {
-        overflowX: 'hidden',
-        overflowY: "scroll",
-        paddingTop: (this.props.style || {paddingTop: 0}).paddingTop,
-        marginTop: this.props.navbarOffset + offsetTop,
-        minHeight: global.innerHeight - this.props.navbarOffset - offsetTop
-      },
-      content: {
-        WebkitOverflowScrolling: "touch"
-      }
-    });
-  }
+	prepareStyles(){
+		let offsetTop = this.getThemeStyles('view').offsetTop || 0;
+		return copy({
+			outer: {
+				position: "relative",
+				top: 0, bottom: 0, left: 0, right: 0,
+				width: "100%", height: "100%"
+			},
+			viewframe: {
+				position: "relative",
+				top: 0, left: 0, right: 0, bottom: 0,
+				width: "100%",
+				overflowX: 'hidden',
+				overflowY: "scroll",
+				minHeight: global.innerHeight,
+				WebkitOverflowScrolling: "touch"
+			},
+			container: {
+				overflowX: 'hidden',
+				overflowY: "scroll",
+				paddingTop: (this.props.style || {paddingTop: 0}).paddingTop,
+				marginTop: this.props.navbarOffset + offsetTop,
+				minHeight: (global.innerHeight - this.props.navbarOffset) - offsetTop
+			},
+			content: {}
+		});
+	}
 });
 
 export default View;
